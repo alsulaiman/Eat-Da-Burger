fb410deeca9e55c821284cefc5ead9926fc0f188
hamad alsulaiman <Hamad@hamadsMacBook-2.local>
2018-09-23 15:03:02 -0400

    update

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// Connecting Node to burgers_db in MySQL 
+// and then exporting the connection
+var mysql = require('mysql');
+var connection;
+
+// Localhost connection
+connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '12345678',
+    database : 'burgers_db',
+    port: 3306
+  });
+
+module.exports = connection; 
